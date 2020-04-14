5b18325b3cf17292af190036c2c4a6f3b4b09208
Mark Dino <markdinopelonia447@gmail.com>
2020-04-14 19:08:29 +0800

    feat: commentSchema

---
+++ models/comment.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const commentSchema = new mongoose.Schema({
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  },
+  message: { type: String, require: [true, "Message field is reuired"] },
+  date: { type: Date, default: Date.now }
+});
+
+const Comment = mongoose.model("Comment", commentSchema);
+
+exports.commentSchema = commentSchema;
+exports.Comment = Comment;
