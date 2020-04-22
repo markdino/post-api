a93ffa94de21337d0d507b8e3b44eb8224035084
Mark Dino <markdinopelonia447@gmail.com>
2020-04-22 15:27:49 +0800

    feat: Authenticate user in like route

--- routes/like.js
+++ routes/like.js
@@ -1,21 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const Post = require("../models/post");
+const auth = require("../middleware/auth");
 
-// Add like to post
-router.put("/:id/like", async (req, res) => {
+// Add/remove like to post
+router.put("/:id/like", auth, async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
-    const user = post.likes.find(
-      like => like.user.toString() === req.body.user
-    );
+    const user = post.likes.find(like => like.user.toString() === req.user._id);
     const liked = user ? false : true;
 
     if (!user) {
-      post.likes.push(req.body);
+      post.likes.push({ user: req.user._id });
     } else {
       post.likes = post.likes.filter(
-        like => like.user.toString() !== req.body.user
+        like => like.user.toString() !== req.user._id
       );
     }
     await post.save();
